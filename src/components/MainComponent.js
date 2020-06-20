5943ad455448016745e401dea378495ce5b51ca0
Vipul Tanwar <vipultanwar45@gmail.com>
2020-06-20 22:27:50 +0530

    Assignment 2: React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import { Navbar, NavbarBrand, Input } from 'reactstrap';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path="/menu" component={ ()=> <Menu dishes={this.state.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
