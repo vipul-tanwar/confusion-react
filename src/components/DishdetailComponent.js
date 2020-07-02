fdb6c2939437a01b0059b26eb2ffdef718f8553d
Vipul Tanwar <vipultanwar45@gmail.com>
2020-07-02 20:36:15 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 
 const required = (val) => val && val.length;
@@ -142,7 +144,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
         if(dish != null){
             return (
                 <Card  >
-                <CardImg width="100%" src={dish.image}  alt={dish.name}></CardImg>
+                <CardImg width="100%" src={baseUrl + dish.image}  alt={dish.name}></CardImg>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,6 +4,8 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,  CardTitle, Breadcru
 import DishDetail from './DishdetailComponent';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 
 
@@ -11,7 +13,7 @@ function RenderMenuItem({ dish, onClick}){
     return(
         <Card > 
             <Link to={`/menu/${dish.id}`} >
-               <CardImg width="100%" src={dish.image}  alt={dish.name}></CardImg>
+               <CardImg width="100%" src={baseUrl + dish.image}  alt={dish.name}></CardImg>
                 <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
                </CardImgOverlay>
